ac7c6ae310ef885422014c06e4f7c52cc0bf649a
aymanEnnajy <med.en1990a@gmail.com>
2025-12-20 19:45:27 +0100

    ajouter id user

--- nodejs_proj.sql
+++ nodejs_proj.sql
@@ -9,16 +9,18 @@ CREATE TABLE IF NOT EXISTS cats (
   tag TEXT DEFAULT NULL,
   description TEXT,
   images TEXT,
+  id_user INTEGER DEFAULT 1,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (id_user) REFERENCES users(id)
 );
 
-INSERT INTO cats (id, name_cats, tag, description, images, created_at, updated_at) VALUES
-(1, 'Whiskers', 'Playful', 'A very playful cat who loves toys and attention', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyaS9dW15PMOAWf9oneAPmq-Fys0W8w3nPXC7wZ36CROlJ3tkNFb0_9uz5CI7h98cHIzOUupljRsENdsXzDLtFfJ-mMB_KHcl7TjZGD8qoXg&s=10', '2025-12-15 02:00:59', '2025-12-16 21:27:01'),
-(3, 'Simba', 'Adventurous', 'Always exploring and curious about everything', 'https://images.unsplash.com/photo-1533738363-b7f9aef128ce?w=400', '2025-12-15 02:00:59', '2025-12-15 02:00:59'),
-(4, 'moon', 'dddd', 'dddd', 'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?w=400', '2025-12-15 03:05:33', '2025-12-15 03:05:56'),
-(5, 'luna', 'oscar', 'mata', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlOYId9yAzW4d7oOb1__QztJahf8hZ_d6naMsnqeBXGry2n-dRxc-X_zhiodnnXxccOs0oXWVBDne7touXKfGOXV-HbH2cZ3TgRfb-fWeQ&s=10', '2025-12-16 21:26:31', '2025-12-16 21:26:31'),
-(6, 'a', 'a', 'a', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyaS9dW15PMOAWf9oneAPmq-Fys0W8w3nPXC7wZ36CROlJ3tkNFb0_9uz5CI7h98cHIzOUupljRsENdsXzDLtFfJ-mMB_KHcl7TjZGD8qoXg&s=10', '2025-12-16 21:27:20', '2025-12-16 21:27:20');
+INSERT INTO cats (id, name_cats, tag, description, images, id_user, created_at, updated_at) VALUES
+(1, 'Whiskers', 'Playful', 'A very playful cat who loves toys and attention', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyaS9dW15PMOAWf9oneAPmq-Fys0W8w3nPXC7wZ36CROlJ3tkNFb0_9uz5CI7h98cHIzOUupljRsENdsXzDLtFfJ-mMB_KHcl7TjZGD8qoXg&s=10', 1, '2025-12-15 02:00:59', '2025-12-16 21:27:01'),
+(3, 'Simba', 'Adventurous', 'Always exploring and curious about everything', 'https://images.unsplash.com/photo-1533738363-b7f9aef128ce?w=400', 1, '2025-12-15 02:00:59', '2025-12-15 02:00:59'),
+(4, 'moon', 'dddd', 'dddd', 'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?w=400', 1, '2025-12-15 03:05:33', '2025-12-15 03:05:56'),
+(5, 'luna', 'oscar', 'mata', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlOYId9yAzW4d7oOb1__QztJahf8hZ_d6naMsnqeBXGry2n-dRxc-X_zhiodnnXxccOs0oXWVBDne7touXKfGOXV-HbH2cZ3TgRfb-fWeQ&s=10', 1, '2025-12-16 21:26:31', '2025-12-16 21:26:31'),
+(6, 'a', 'a', 'a', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyaS9dW15PMOAWf9oneAPmq-Fys0W8w3nPXC7wZ36CROlJ3tkNFb0_9uz5CI7h98cHIzOUupljRsENdsXzDLtFfJ-mMB_KHcl7TjZGD8qoXg&s=10', 1, '2025-12-16 21:27:20', '2025-12-16 21:27:20');
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
